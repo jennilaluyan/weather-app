f48eadbce29bc015a36feb488486b7a719868c12
jennilaluyan <laluyanjennifer11@gmail.com>
2025-01-25 23:24:03 +0800

    late adjustments from tutorial

--- script.js
+++ script.js
@@ -10,28 +10,29 @@ async function checkWeather(city) {
     if (response.status == 404) {
         document.querySelector(".error").style.display = "block"
         document.querySelector(".weather").style.display = "none"
+    } else {
+        var data = await response.json()
+
+        document.querySelector(".city h2").textContent = data.name
+        document.querySelector(".temperature h1").textContent = Math.round(data.main.temp) + "°c"
+        document.querySelector(".humidity h3").textContent = data.main.humidity + "%"
+        document.querySelector(".wind-speed h3").textContent = data.wind.speed + " km/h"
+
+        if (data.weather[0].main === "Clouds") {
+            weatherIcon.src = "./assets/cloudy.png"
+        } else if (data.weather[0].main === "Clear") {
+            weatherIcon.src = "./assets/clear.png"
+        } else if (data.weather[0].main === "Rain") {
+            weatherIcon.src = "./assets/rain.png"
+        } else if (data.weather[0].main === "Drizzle") {
+            weatherIcon.src = "./assets/drizzle.png"
+        } else if (data.weather[0].main === "Mist") {
+            weatherIcon.src = "./assets/mist.png"
+        }
+
+        document.querySelector(".weather").style.display = "block"
+        document.querySelector(".error").style.display = "block"
     }
-
-    var data = await response.json()
-
-    document.querySelector(".city h2").textContent = data.name
-    document.querySelector(".temperature h1").textContent = Math.round(data.main.temp) + "°c"
-    document.querySelector(".humidity h3").textContent = data.main.humidity + "%"
-    document.querySelector(".wind-speed h3").textContent = data.wind.speed + " km/h"
-
-    if (data.weather[0].main === "Clouds") {
-        weatherIcon.src = "./assets/cloudy.png"
-    } else if (data.weather[0].main === "Clear") {
-        weatherIcon.src = "./assets/clear.png"
-    } else if (data.weather[0].main === "Rain") {
-        weatherIcon.src = "./assets/rain.png"
-    } else if (data.weather[0].main === "Drizzle") {
-        weatherIcon.src = "./assets/drizzle.png"
-    } else if (data.weather[0].main === "Mist") {
-        weatherIcon.src = "./assets/mist.png"
-    }
-
-    document.querySelector(".weather").style.display = "block"
 }
 
 searchButton.addEventListener("click", function(){
